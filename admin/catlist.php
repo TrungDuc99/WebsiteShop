09619e48acb5e6ab597443cde19a2cbbf212ef08
TrungDuc <dotrungduc.bl@gmail.com>
2021-04-07 08:27:36 +0700

    add file project website

---
+++ admin/catlist.php
@@ -0,0 +1,67 @@
+﻿<?php include 'inc/header.php';?>
+<?php include 'inc/sidebar.php';?>
+<?php include '../classes/category.php';?>
+
+<?php
+	$cat = new category(); // tên class trong file .ok thanks nha
+	//load tren 1 trang isset no ko co lay dc gia tri, vi bien delid da bi xoa khi load lai
+	if ($_GET['delid']!= null) {
+		$id = $_GET['delid'];
+		//var_dump($id);
+		 $delcat = $cat->del_category($id);
+	}
+?>
+
+        <div class="grid_10">
+            <div class="box round first grid">
+                <h2>Category List</h2>
+                <div class="block">       
+				<?php
+
+                if(isset($delcat)){
+                    echo $delcat;
+				}
+				
+                ?> 
+                    <table class="data display datatable" id="example">
+					<thead>
+						<tr>
+							<th>Serial No.</th>
+							<th>Category Name</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php
+						$show_cate = $cat->show_category();
+						if($show_cate){
+							$i = 0;
+							while($result = $show_cate->fetch_assoc()){
+							$i++;
+						
+					?>
+
+						<tr class="odd gradeX">
+							<td><?php echo $i?></td>
+							<td><?php echo $result['catName']?></td>
+							<td><a href="catedit.php?catId=<?php echo $result['catId']?>">Edit</a> ||<a onclick=" return confirm('Are you sure you want to delete this')" href="?delid=<?php echo $result['catId']?>">Delete</td>
+						</tr>
+						<?php
+							}
+						}
+						?>
+					</tbody>
+				</table>
+               </div>
+            </div>
+        </div>
+<script type="text/javascript">
+	$(document).ready(function () {
+	    setupLeftMenu();
+
+	    $('.datatable').dataTable();
+	    setSidebarHeight();
+	});
+</script>
+<?php include 'inc/footer.php';?>
+
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+	include './inc/header.php';
+	include './inc/slider.php';
+?>
+ <div class="main">
+    <div class="content">
+    	<div class="content_top">
+    		<div class="heading">
+    		<h3>Feature Products</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+	      <div class="section group">
+		  	<?php
+			  	
+			  	$product_feathered = $product->getproduct_feathered();
+			  	if($product_feathered){ 
+					  while($result = $product_feathered->fetch_assoc()){  
+
+			  ?>
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="preview.html"><img src="admin/uploads/<?php echo $result['image']?>" alt="" /></a>
+					 <h2><?php echo $result['productName']?> </h2>
+					 <p><?php echo $fm->textShorten($result['product_desc'],50)?></p>
+					 <p><span class="price"><?php echo $result['price']." "."VND"?></span></p>
+				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
+				</div>
+				<?php
+					  }
+					}
+				?>
+			</div>
+			<div class="content_bottom">
+    		<div class="heading">
+    		<h3>New Products</h3>
+    		</div>
+    		<div class="clear"></div>
+    	</div>
+			<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="preview.html"><img src="images/new-pic1.jpg" alt="" /></a>
+					 <h2>Lorem Ipsum is simply </h2>
+					 <p><span class="price">$403.66</span></p>
+				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="preview.html"><img src="images/new-pic2.jpg" alt="" /></a>
+					 <h2>Lorem Ipsum is simply </h2>
+					 <p><span class="price">$621.75</span></p> 
+				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="preview.html"><img src="images/feature-pic2.jpg" alt="" /></a>
+					 <h2>Lorem Ipsum is simply </h2>
+					 <p><span class="price">$428.02</span></p>
+				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+				 <img src="images/new-pic3.jpg" alt="" />
+					 <h2>Lorem Ipsum is simply </h2>					 
+					 <p><span class="price">$457.88</span></p>
+
+				     <div class="button"><span><a href="preview.html" class="details">Details</a></span></div>
+				</div>
+			</div>
+    </div>
+ </div>
+ 
+<?php
+	include './inc/footer.php';
+	
+?>
+
+
+ 
